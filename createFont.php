90a6c58a4cf6fdb4ac5ec6e3aeb44bc85b8100f2
Md. Al-Amin <41773444+alamin22@users.noreply.github.com>
2020-02-28 04:23:12 -0800

    Update createFont.php

--- createFont.php
+++ createFont.php
@@ -4,9 +4,12 @@
 3.Generate font from sutonnymj.ttf file with  command mode as
   path/tcpdf/fonts/tcpdf_addfont.php  -t sutonnymj.ttf.
 if you success then create three file in your path directory
-such as sutonnymj.ctg.z, sutonnymj.z, sutonnymj.php
+such as 
+sutonnymj.ctg.z, 
+sutonnymj.z,
+sutonnymj.php
 sutonnymj.php that you can use font name in tcpdf pdf code
-4. you need a converter to convert bangla sutonnymj font that use as bijoy font .
+4. you need to create a converter to convert bangla sutonnymj font that use as bijoy font .
 example lang -> bn
 <?php
 include"converter.php";
